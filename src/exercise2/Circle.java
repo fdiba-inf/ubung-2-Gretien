6a7f28c5fb127d44e093b012dfd23c20acd401b8
repl.it user <replituser@example.com>
2020-10-08 15:03:56 +0000

    Circle

--- src/exercise2/Circle.java
+++ src/exercise2/Circle.java
@@ -7,13 +7,10 @@ public class Circle {
   public static void main (String[] args) {
     Scanner input = new Scanner(System.in);
 
-    System.out.print("Enter radius:");
     int number = input.nextInt();
-    double c = Math.PI;
-    double a = 2*c*number;
-    double b = c*number*number;
-    System.out.print("Circumference:"+ a +"   Area:" + b);
-
-
+    double a = 2*Math.PI*number;
+    double b = Math.PI*number*number;
+    System.out.println("Circumference:"+ a);
+    System.out.println("Area:"+ b);
   }
 }
